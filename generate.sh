f805eec7b9b5143c9a962e305ff0e7d6c299f17a
Alexander Gusev <alexander.gusev@promwad.com>
2021-08-20 17:06:40 +0300

    Add webp animated images

--- generate.sh
+++ generate.sh
@@ -7,7 +7,23 @@ for file in $SOURCE/*
 do
     filename=$(basename "${file%.*}")
     echo Rendering $filename
-    ffmpeg -loglevel warning -y -i $file -ss 00:00:00 -t 00:00:05 -aspect 16:9 -s 512x288 $OUT/$filename.mp4
+    # ffmpeg -loglevel warning -y -i $file -ss 00:00:00 -t 00:00:05 -aspect 16:9 -s 512x288 $OUT/$filename.mp4
+    ffmpeg \
+        -y \
+        -i $file \
+        -vcodec libwebp \
+        -filter:v fps=fps=10 \
+        -lossless 0 \
+        -compression_level 3 \
+        -q:v 70 \
+        -loop 1 \
+        -preset picture \
+        -an \
+        -vsync 0 \
+        -s 512:288 \
+        -ss 00:00:00 \
+        -t 00:00:05 \
+        $OUT/$filename.webp
 done
 
 echo Done
