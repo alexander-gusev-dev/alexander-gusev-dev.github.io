2e711bb7e04d52d99b0943be9cf4e03f7c51fcf1
Alexander Gusev <alexander.gusev@promwad.com>
2021-08-20 17:59:46 +0300

    Remove MP4, update webp

--- generate.sh
+++ generate.sh
@@ -7,7 +7,11 @@ for file in $SOURCE/*
 do
     filename=$(basename "${file%.*}")
     echo Rendering $filename
+
+    #MP4
     # ffmpeg -loglevel warning -y -i $file -ss 00:00:00 -t 00:00:05 -aspect 16:9 -s 512x288 $OUT/$filename.mp4
+
+    #WEBP animated image
     ffmpeg \
         -y \
         -loglevel warning \
@@ -16,15 +20,16 @@ do
         -filter:v fps=fps=12 \
         -lossless 0 \
         -compression_level 3 \
-        -q:v 60 \
+        -q:v 10 \
         -loop 0 \
         -preset picture \
         -an \
         -vsync 0 \
         -s 512:288 \
         -ss 00:00:05 \
-        -t 00:00:10 \
+        -t 00:00:05 \
         $OUT/$filename.webp
+
 done
 
 echo Done
